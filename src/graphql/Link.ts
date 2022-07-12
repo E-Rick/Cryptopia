b0a9816608cf7dd24086c928037dc98d55583d45
Erick Martinez <erick@wrecs.studio>
2022-07-12 16:22:04 -0400

    add: CRUB functionality to Links

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from 'nexus';
+import { extendType, objectType, nonNull, stringArg, intArg, arg } from 'nexus';
 import { NexusGenObjects } from '../../nexus-typegen';
 
 export const Link = objectType({
@@ -23,6 +23,84 @@ export const LinkQuery = extendType({
 			type: 'Link',
 			resolve(parent, args, context, info) {
 				return links;
+			},
+		});
+
+		// Fetch a single link by it's id
+		t.field('link', {
+			type: 'Link',
+			args: {
+				id: nonNull(intArg()),
+			},
+		});
+	},
+});
+
+//  Create a mutation for adding new links
+
+export const LinkMutation = extendType({
+	type: 'Mutation', // extend Mutation type to add new root field
+	definition(t) {
+		// Post a new link
+		t.nonNull.field('post', {
+			// the name of the mutation defined as post and returns non nullable link object
+			type: 'Link',
+			args: {
+				description: nonNull(stringArg()),
+				url: nonNull(stringArg()),
+			},
+
+			resolve(parent, args, context) {
+				const { description, url } = args;
+
+				let idCount = links.length + 1; //5
+				const link = {
+					id: idCount,
+					description: description,
+					url: url,
+				};
+				links.push(link);
+				return link;
+			},
+		});
+
+		// Update a link
+		t.nonNull.field('updateLink', {
+			type: 'Link',
+			args: {
+				id: nonNull(intArg()),
+				url: stringArg(),
+				description: stringArg(),
+			},
+
+			resolve(_, args, context) {
+				const { id, description, url } = args;
+
+				let link = links[id - 1];
+
+				// check if description or url exists
+				link = {
+					id: link.id,
+					description: args.description || link.description,
+					url: args.url || link.url,
+				};
+				links[id] = link;
+
+				return link;
+			},
+		});
+
+		// Delete a link
+		t.nonNull.field('deleteLink', {
+			type: 'Link',
+			args: {
+				id: nonNull(intArg()),
+			},
+
+			resolve(_, args, context) {
+				const link = links[args.id - 1];
+				links.splice(args.id - 1, 1);
+				return link;
 			},
 		});
 	},
